73d00d2adb55d3cd9b19935fba760ad4cf36d3e2
Kyle Richardson <s.k.richardson7@gmail.com>
2015-06-01 22:00:01 -0400

    Checked JS/SCSS/ and gulp working properly

---
+++ dist/scripts/templates.js
@@ -0,0 +1,4 @@
+this["JST"] = this["JST"] || {};
+this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Hello</h1>\n";
+},"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 (function(){
   'use strict';
 
+alert('hello');
+
+
   $(document).ready(function(){
     $('body').prepend(JST['application']());
   });
